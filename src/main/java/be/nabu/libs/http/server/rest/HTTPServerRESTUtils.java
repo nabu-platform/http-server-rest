a15e62fad135f6d918924b6c9d6fbca40ec46096
alex <alex@nabu.be>
2015-11-02 10:56:30 +0100

    Pom management

--- src/main/java/be/nabu/libs/http/server/rest/HTTPServerRESTUtils.java
+++ src/main/java/be/nabu/libs/http/server/rest/HTTPServerRESTUtils.java
@@ -14,7 +14,7 @@ public class HTTPServerRESTUtils {
 	}
 	
 	public static void handleRest(HTTPServer server, Class<?> restClass, String serverPath, RoleHandler roleHandler, Object...context) {
-		server.getEventDispatcher().subscribe(HTTPRequest.class, restHandler(restClass, serverPath, roleHandler, context))
+		server.getDispatcher(null).subscribe(HTTPRequest.class, restHandler(restClass, serverPath, roleHandler, context))
 			.filter(HTTPServerUtils.limitToPath(serverPath));
 	}
 }
